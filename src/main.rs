6572954d97dc10d3a3eed5582228f9ea68cf67d0
Zero <1300326388@qq.com>
2023-09-06 17:01:39 +0800

    1、增加测试和自动化 Build

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use time::OffsetDateTime;
+extern crate regex;
 use rand::prelude::*;
+use regex::Regex;
+use time::OffsetDateTime;
 
 fn main() {
     let n = 80; // 计算第40个斐波那契数
     println!("Rust Fibonacci({}): {}", n, fibonacci(n));
-    let now = OffsetDateTime::now_utc();
+    let now: OffsetDateTime = OffsetDateTime::now_utc();
     println!("Now: {}", now);
     let local: OffsetDateTime = now.to_offset(now.offset());
     println!("Local: {}", local);
     let x: u8 = random();
-    let mut rng=rand::thread_rng();
+    let mut rng = rand::thread_rng();
     let y: u8 = rng.gen_range(1..100);
     println!("x:{x} y:{y}");
-
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
 
 fn fibonacci(n: u64) -> u64 {
@@ -30,4 +33,14 @@ fn fibonacci(n: u64) -> u64 {
         b = temp;
     }
     b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(fibonacci(80), 23416728348467685);
+    }
 }
